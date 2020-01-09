61f88b254579f03ef93ec99682192d57e04fba11
OE10x2 <OE10x2@gmail.com>
2020-01-09 09:04:21 -0500

    Got rid of lighting

--- src/legacyGL.java
+++ src/legacyGL.java
@@ -85,32 +85,6 @@ public class legacyGL{
 		glEnable(GL_TEXTURE_2D);
 		glEnable(GL_SMOOTH);
 		glEnable(GL_DEPTH_TEST);
-		// set up lighting
-		FloatBuffer ambient = BufferUtils.createFloatBuffer(4);
-		ambient.put(new float[] { 0.6f, 0.65f, 0.65f, 1f, });
-		ambient.flip();
-		FloatBuffer specular = BufferUtils.createFloatBuffer(4);
-		specular.put(new float[] { 0.8f, 0.8f, 0.8f, 1f, });
-		specular.flip();
-		FloatBuffer position = BufferUtils.createFloatBuffer(4);
-		position.put(new float[] { 0f, 5f, -5f, 1f, });
-		position.flip();
-		FloatBuffer spot_dir = BufferUtils.createFloatBuffer(4);
-		spot_dir.put(new float[] { 0f, -5f, -5f, 0f, });
-		spot_dir.flip();
-
-		/*
-		//Default Lighting; NOT USED
-		glEnable(GL_LIGHTING);
-		glEnable(GL_LIGHT0);
-		glLightModelfv(GL_LIGHT_MODEL_AMBIENT, ambient);
-		glLightModeli(GL_LIGHT_MODEL_LOCAL_VIEWER, GL_FALSE);
-		glLightfv(GL_LIGHT0, GL_POSITION, position);
-		glLightfv(GL_LIGHT0, GL_DIFFUSE, specular);
-		glLightfv(GL_LIGHT0, GL_SPOT_DIRECTION, spot_dir);
-		glLightf(GL_LIGHT0, GL_SPOT_CUTOFF, 45.0f);
-		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-		 */
 
 		objects.add(new MeshObject("Resource/Models/NMap.obj"));
 
@@ -138,7 +112,6 @@ public class legacyGL{
 		else if (movement[1]) tz -= 0.1;
 		if (movement[2]) tx += 0.1;
 		else if (movement[3]) tx -= 0.1;
-		System.out.println(tx + " " + ty + " " + tz);
 		//Note: tx is for left/right, tz is for forward/back
 		glTranslatef(tx, ty, tz); //NOTE: Only the x value changes; the height never changes
 		for (MeshObject object: objects) object.draw(); //Draw the objects
