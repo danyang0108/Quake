1acab64698d0172154eb0270d2118d811fde61e5
danyang0108 <56311627+danyang0108@users.noreply.github.com>
2020-01-15 13:59:16 -0500

    Add files via upload

---
+++ src/BFS.java
@@ -0,0 +1,19 @@
+
+
+public class BFS {
+	
+	int[][] vis = new int[42][25];
+	int[][] dis = new int[42][25];
+	int d[][] = {{2,0},{-2,0},{0,-1},{0,1}};
+	MyArrayQueue q = new MyArrayQueue();
+	public void bfs(int start_x, int start_y, int end_x, int end_y) {
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+}
