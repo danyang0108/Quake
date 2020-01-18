83f4a7da83c49d2cd860da6212eb0b8ac3b846af
OE10x2 <OE10x2@gmail.com>
2020-01-18 12:05:53 -0500

    General V15 (Added ArrayList.java Interface)

---
+++ src/AL.java
@@ -0,0 +1,7 @@
+public interface AL<T>{
+    public void add(T n);
+    public void removeFront();
+    public void replaceNode(T n, int i);
+    public T get(int i);
+    public int size();
+}
--- src/ArrayList.java
+++ src/ArrayList.java
@@ -1,31 +1,31 @@
-public class ArrayList<T>{
+public class ArrayList<T> implements AL<T>{
     private int size = 20001; //Default size
     private int length = 0; //Tracks the actual length of the ArrayList
     private int fix = 0; //Tracks the number of elements removed from the front
     private Node<T>[] array = new Node[size];
 
-    public void add(T n){
+    @Override public void add(T n){
         //We also add "fix" to the index; read document for details
         array[fix + length++] = new Node<>(n); //Convert n into a Node
     }
 
-    public void removeFront(){
+    @Override public void removeFront(){
         //Removes the first element in the ArrayList
         fix++; //One more element at the front is not used
         length = Math.max(length - 1, 0); //Ensure that the value of length is non-negative
     }
 
-    public void replaceNode(T n, int i){
+    @Override public void replaceNode(T n, int i){
         //Change the value of the Node
         array[i + fix] = new Node<>(n);
     }
 
-    public T get(int i){
+    @Override public T get(int i){
         //Returns the value at the given index
         return array[i + fix].getValue();
     }
 
-    public int size(){
+    @Override public int size(){
         //Returns the length of the ArrayList
         return length;
     }
